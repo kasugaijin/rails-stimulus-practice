2cb4a1f145af0ed2c3f2d2eab7797d4dd0f6070d
Ben <robinsonbena@gmail.com>
2022-06-27 13:53:38 -0700

    add function to replace item i.e., click on it and it disappears and another item appears and so on;

--- app/javascript/controllers/toggle_controller.js
+++ app/javascript/controllers/toggle_controller.js
@@ -1,11 +1,25 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-  static targets = ['hideable']
+  static targets = ['hideable', 'first', 'second']
 
   toggle() {
-    let cssClass = this.hideableTarget.classList
+    let cssClass = this.hideableTarget.classList;
     cssClass == 'hidden' ? cssClass.remove('hidden') : cssClass.add('hidden');
     };
+
+  replace() {
+    let first = this.firstTarget.classList;
+    let second = this.secondTarget.classList;
+    
+    if (second == 'hidden') {
+      first.add('hidden');
+      second.remove('hidden');
+    }
+    else {
+      first.remove('hidden')
+      second.add('hidden')
+    }
+  }
   
 }
