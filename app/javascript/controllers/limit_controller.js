d1ffd530d11036d5ee6255a4600f593de4e88134
Ben <robinsonbena@gmail.com>
2022-06-28 14:49:34 -0700

    text area character counter complete - turns red if 5 or fewer chars left

--- app/javascript/controllers/limit_controller.js
+++ app/javascript/controllers/limit_controller.js
@@ -4,9 +4,11 @@ export default class extends Controller {
   static targets = ['input', 'output']
 
   limit() {
-    let counter = this.inputTarget.value.length;
-    console.log(counter)
-    this.outputTarget.innerText = counter;
+    let output = this.outputTarget
+    let counter = 50 - this.inputTarget.value.length;
+    output.innerText = counter;
+
+    counter <= 5 ? output.style.cssText='color: red;' : output.style.cssText='color:blue;'
   }
 
 }
